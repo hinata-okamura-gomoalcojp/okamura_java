fb3cbbd52f26e035773a93d20cc6fd4494049a79
”hinata-okamura-gomoalcojp” <hinata.okamura@gomoal.co.jp>
2023-05-19 16:24:47 +0900

    ファーストコミット

---
+++ src/main/java/okamura/oonavi_java_springboot/OonaviJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package okamura.oonavi_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OonaviJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OonaviJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/okamura/oonavi_java_springboot/OonaviJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package okamura.oonavi_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OonaviJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
